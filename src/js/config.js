d034c1410f1737818b76da717d5bf08352f96da6
TranNgocCong <55443845+TranNgocCong@users.noreply.github.com>
2021-01-09 20:36:48 +0700

    fix_st

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'd3d6217f-483c-4d4b-864d-b5efa663e595';
+export const KEY = '549fe345-c79e-4dad-9edb-2cfb9e288ec8';
 export const MODAL_CLOSE_SEC = 2.5;
